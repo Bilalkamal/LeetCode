e49a020a64d826b0bd8914f7b409c64455d0c523
Bilal K. Hamada <Bilalkamal@users.noreply.github.com>
2022-05-15 06:33:19 +0200

    Time: 34 ms (78.91%), Space: 14 MB (24.66%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        print(s)
+        stack = []
+        pars = {"(":")", "{":"}", "[":"]"}
+        for p in s:
+            if p in pars.keys():
+                stack.append(pars[p]) 
+            else:
+                if not stack:
+                    return False
+                elif p != stack[-1]:   
+                    return False
+                else:
+                    stack.pop()
+        if stack:
+            return False
+        return True
