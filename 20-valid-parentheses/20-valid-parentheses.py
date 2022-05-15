a93459fb633cce474289b11182d53f13e25de688
Bilal K. Hamada <Bilalkamal@users.noreply.github.com>
2022-05-15 06:35:04 +0200

    Time: 60 ms (14.86%), Space: 13.9 MB (70.75%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -6,12 +6,15 @@ class Solution:
             if p in pars.keys():
                 stack.append(pars[p]) 
             else:
+#               if stack is empty or the last item is not the similar, return False.
+#               otherwise, pop!
                 if not stack:
                     return False
                 elif p != stack[-1]:   
                     return False
                 else:
                     stack.pop()
+#       if stack still has some items
         if stack:
             return False
         return True
