e5b6fdd401194e2e73615ed081330e5a8a5d71b1
Bilal K. Hamada <Bilalkamal@users.noreply.github.com>
2022-05-19 13:58:54 +0200

    Time: 36 ms (91.66%), Space: 13.8 MB (57.29%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -3,7 +3,7 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x <=1:
             return x
-        x_n = 0.5 * x 
+        x_n = 0.3 * x 
         diff = 1
         while diff > 0.1:
             next_x = (0.5 * (x_n + x/x_n))
